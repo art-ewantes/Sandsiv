ca829c07e34553848fb52e38ec93cd9554474377
art-ewantes <web-rawwwr@gmail.com>
2017-01-11 23:55:05 +0200

    last commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	$(".navbar-fixed-top").autoHidingNavbar({
+		animationDuration: 400,
+		hideOffset: 60
+	});
 
 	// $("#toggle_mnu").click(function(){
 	// 	$(this).toggleClass("on");
@@ -9,11 +13,12 @@ $(document).ready(function() {
 	$('.main_carousel .owl-carousel').owlCarousel({
 	    loop:true,
 	    margin:10,
-	    autoplayTimeout: 4000,
+	    autoplayTimeout: 4000000,
 	    autoplaySpeed:1400,
 	    smartSpeed: 600,
 	    pagination:true,
 	    nav:true,
+	    autoplayHoverPause: true,
 	    navText: ['<img src="../img/prev.png"/>','<img src="../img/next.png"/>'],
 	    dots:false,
 	    responsive:{
@@ -70,7 +75,95 @@ $(document).ready(function() {
 	        }
 	    }
 	});
-	// $("input, select, textarea").jqBootstrapValidation();
+	$('.clients .owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 5,
+	    margin:10,
+	    autoplayTimeout: 4000,
+	    autoplaySpeed:1400,
+	    smartSpeed: 600,
+	    pagination:false,
+	    nav:false,
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1,
+	            pagination: true,
+	            dots: true
+	        },
+	        600:{
+	            items:2,
+	            autoplay: true,
+	            pagination: true,
+	            dots: true
+	        },
+	        700:{
+	            items:3,
+	            autoplay: true,
+	            pagination: true,
+	            dots: true
+	        },
+	        900:{
+	            items:3,
+	            autoplay: true,
+	            pagination: false,
+	            dots: false
+	        },
+	        1200:{
+	        	items: 5,
+	        	autoplay: true,
+	        }
+	    }
+	});
+	$('.our_partners .owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    margin: 20,
+	    autoplayTimeout: 40000000,
+	    autoplaySpeed:1400,
+	    smartSpeed: 600,
+	    pagination:false,
+	    nav:false,
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1,
+	            pagination: true,
+	            dots: true
+	        },
+	        600:{
+	            items:2,
+	            autoplay: true,
+	            pagination: true,
+	            dots: true
+	        },
+	        700:{
+	            items:2,
+	            autoplay: true,
+	            pagination: true,
+	            dots: true
+	        },
+	        900:{
+	            items:2,
+	            autoplay: true,
+	            pagination: true,
+	            dots: true
+	        },
+	        1000:{
+	            items:3,
+	            autoplay: true,
+	            pagination: false,
+	            dots: false
+	        },
+	        1200:{
+	        	items: 4,
+	        	autoplay: true,
+	        	pagination: false,
+	            dots: false
+	        }
+	    }
+	});
+	$("input, select, textarea").jqBootstrapValidation();
 
 	$(".search").click(function(){
 		$(".overlay-search").toggleClass("focus-search");
@@ -81,12 +174,41 @@ $(document).ready(function() {
 		$(".overlay-search").removeClass("focus-search");
 	});
 
+	// $(document).ready(function(){
+	//     $(".item_row_tech").height( $(".row_item").height() );
+	// });
 
-
-
+	
 
 
 });
 
+// $(function(){
+//     $(window).scroll(function() { 
+//         if ($(this).scrollTop() > 75) { 
+//             $("#ddmenubg2:hidden").css('visibility','visible');   
+//             $("#ddmenubg2:hidden").fadeIn('slow');  
+//         } 
+//         else {     
+//             $("#ddmenubg2:visible").fadeOut("slow"); 
+//         }  
+//     });
+// });
 
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
+     //>=, not <=
+    if (scroll >= 1) {
+        //clearHeader, not clearheader - caps H
+        $(".header_top_line").addClass("hidden_bar");
+        $(".header").addClass("castombar");
+    }else if(scroll <= 50){
+    	$(".header_top_line").removeClass("hidden_bar");
+    	$(".header").removeClass("castombar");
+    }
+    // if (scroll >= 50) {
+    //     //clearHeader, not clearheader - caps H
+    //     $(".navbar").addClass("reloadcss");
+    // }
+}); //missing );
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp"),
 	sass = require("gulp-sass"),
+	gutil = require( 'gulp-util' ),  
+	ftp = require( 'vinyl-ftp' ),
 	browserSync = require("browser-sync"),
 	concat = require("gulp-concat"),
 	uglify = require("gulp-uglifyjs"),
@@ -29,6 +31,7 @@ gulp.task("scripts", function(){
 		"app/libs/scroll2id/PageScroll2id.min.js",
 		"app/libs/bootstrap/dist/js/bootstrap.min.js",
 		"app/libs/owl.carousel/dist/owl.carousel.min.js",
+		"app/libs/header_bs3/dist/jquery.bootstrap-autohidingnavbar.min.js",
 		])
 	.pipe(concat("libs.min.js"))
 	.pipe(uglify())
@@ -95,4 +98,65 @@ gulp.task("build", ["clean", "img", "sass", "scripts"], function(){
 	.pipe(gulp.dest("dist"));
 
 
+});
+
+/** Configuration ftp**/
+var user = 'dimaproz@web-rawwwr.com';  
+var password = 'getthere1995';  
+var host = 'ftp.web-rawwwr.com';  
+var port = 21;  
+var localFilesGlob = [
+	'dist/css/**',
+];  
+var remoteFolder = 'public_html/sandsiv.web-rawwwr.com'
+// var remoteFolder = 'sandsiv-origin.web-rawwwr.com/wp-content/themes/sandsiv'
+
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {  
+    return ftp.create({
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        parallel: 5,
+        log: gutil.log
+    });
+}
+
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy`
+ */
+gulp.task('deploy', function() {
+
+    var conn = getFtpConnection();
+
+    return gulp.src(localFilesGlob, { base: './dist/', buffer: false })
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files 
+        .pipe( conn.dest( remoteFolder ) )
+    ;
+});
+
+/**
+ * Watch deploy task.
+ * Watches the local copy for changes and copies the new files to the server whenever an update is detected
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy-watch`
+ */
+gulp.task('ftp-deploy-watch', function() {
+
+    var conn = getFtpConnection();
+
+    gulp.watch(localFilesGlob)
+    .on('change', function(event) {
+      console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+
+      return gulp.src( [event.path], { base: '.', buffer: false } )
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files 
+        .pipe( conn.dest( remoteFolder ) )
+      ;
+    });
 });
